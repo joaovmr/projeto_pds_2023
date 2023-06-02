5519585d7c4fba06e918a6591b74270c8367d993
Roberto Rosmaninho <robertogomes@dcc.ufmg.br>
2023-06-02 19:36:23 -0300

    Adding Register return form

--- frontend/src/components/accounts/Register.js
+++ frontend/src/components/accounts/Register.js
@@ -11,7 +11,7 @@ const Register = () => {
     const [password2,setPassword2] = useState("")
     const auth = useSelector(state => state.auth)
     const dispatch = useDispatch()
-    
+
     const createUser = e => {
         e.preventDefault()
         if(password && password2 && password===password2){
@@ -22,7 +22,63 @@ const Register = () => {
         setPassword("")
         setPassword2("")
         setUsername("")
+
     }
+    const renderForm = () => {
+        return (
+            <Card style={{width:'40rem'}}>
+                <Card.Body>
+                    <Form onSubmit={createUser}>
+                        <Form.Group as={Row} controlId="username">
+                            <Form.Label column sm={2}>
+                                Usuário
+                            </Form.Label>
+                            <Col sm={10} xs={10}>
+                                <Form.Control
+                                    type="username"
+                                    placeholder ="Insira o usuário"
+                                    value={username}
+                                    onChange={(e) => setUsername(e.target.value)}
+                                />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} controlId="password1">
+                            <Form.Label column sm={2}>
+                            Senha
+                            </Form.Label>
+                            <Col sm={10} xs={10}>
+                            <Form.Control
+                                type="password"
+                                placeholder="Senha"
+                                value={password}
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                            </Col>
+                        </Form.Group>
+
+                        <Form.Group as={Row} controlId="password2">
+                            <Form.Label column sm={2}>
+                            Confirme a senha
+                            </Form.Label>
+                            <Col sm={10} xs={10}>
+                            <Form.Control type="password" placeholder="Confirme a senha"
+                            value={password2}
+                            onChange={(e) => setPassword2(e.target.value)}/>
+                            </Col>
+                        </Form.Group>
+                        <Button variant="primary" type="submit">
+                            Crie sua conta
+                        </Button>
+                    </Form>
+                </Card.Body>
+            </Card>
+        )
+    }
+
+    return (<> {auth.isAuthenticated ? <Redirect to="/" /> :renderForm()}</>
+
+    )
 }
 
 export default Register
